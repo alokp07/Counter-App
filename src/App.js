4196138263f93852d46956d49a918b256e5b6053
alokp07 <alokputhiyottil@gmail.com>
2023-11-15 14:13:26 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-items-center flex-column w-100 vh-100 bg-black">
+       <div className='d-flex justify-content-center align-items-center flex-column p-5 rounded' style={{backgroundColor:'white'}}>
+          <h3 className='text-primary'>Counter Application</h3>
+         <Counter/>
+        </div>
     </div>
   );
 }
